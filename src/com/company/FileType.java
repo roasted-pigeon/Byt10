22a0405963bc3557ecc848d90c8874bd636a1cbc
roasted-pigeon <61635874+roasted-pigeon@users.noreply.github.com>
2023-01-19 15:26:55 +0100

    commit 2 commit

---
+++ src/com/company/FileType.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class FileType {
+    private String name;
+    private String description;
+    private PersonalFile file;
+
+    public FileType(String name, String description, PersonalFile file){
+        this.name = name;
+        this.description = description;
+        this.file = file;
+    }
+}
